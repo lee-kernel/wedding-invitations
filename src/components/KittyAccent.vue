eb185074320dee59d755b78bdffde10f7fcdc089
GoodPerson <494644965@qq.com>
2026-09-22 09:32:32 +0800

    Add highlights to decorative bows

--- src/components/KittyAccent.vue
+++ src/components/KittyAccent.vue
@@ -20,6 +20,7 @@ const kittyImage = `${import.meta.env.BASE_URL}images/invitation/hello-kitty.svg
     <path class="kitty-bow-lobe" d="M57 31C72 10 94 12 95 30c1 17-19 25-40 12Z" />
     <circle class="kitty-bow-knot" cx="50" cy="35" r="16" />
     <path class="kitty-bow-fold" d="m35 28 7 5m23-5-8 6" />
+    <path class="kitty-bow-shine" d="M13 23c3-6 9-8 15-5m44 2c6-2 12 1 15 6M45 27c3-2 7-2 10 0" />
   </svg>
 
   <img v-else class="kitty-accent" :class="`kitty-accent--${variant}`" :src="kittyImage" alt="" draggable="false" />
